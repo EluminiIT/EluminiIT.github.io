5f703754a973b3ee304d504674f2378107eb5dd5
Romulo Marques <romulo.gomes@elumini.com.br>
2023-06-15 16:59:22 -0300

    mapas novos

--- maps/Shark Attack.tsx
+++ maps/Shark Attack.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.0" name="Shark Attack" tilewidth="32" tileheight="32" tilecount="2" columns="2">
- <image source="../tilesets/sharkattack.png" trans="ff00ff" width="64" height="32"/>
+<tileset version="1.9" tiledversion="1.9.0" name="nomes esquadroes" tilewidth="32" tileheight="32" tilecount="200" columns="20">
+ <image source="../tilesets/nomes esquadroes.png" width="640" height="320"/>
 </tileset>
