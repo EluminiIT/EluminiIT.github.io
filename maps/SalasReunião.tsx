73221f3cdf6fb173134a21c61b4527cebccb1924
unknown <danielbadra17@gmail.com>
2024-07-31 15:05:27 -0300

    Alterações no mapa: criação sala marketing e nomeação das salas de reunião

---
+++ maps/SalasReunião.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="SalasReunião" tilewidth="32" tileheight="32" tilecount="3604" columns="53">
+ <image source="../tilesets/SalasReunião.png" width="1700" height="2200"/>
+</tileset>
