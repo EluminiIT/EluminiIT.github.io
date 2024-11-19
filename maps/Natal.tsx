28c1b60c1feb39f3b29171f1f61530720aa0722f
Daniel Badra <danielbadra17@gmail.com>
2024-11-19 10:23:29 -0300

    apaga o arquivo natal.tsx

--- maps/Natal.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Natal" tilewidth="32" tileheight="32" tilecount="1980" columns="60">
- <image source="../tilesets/Natal.png" trans="ff00ff" width="1920" height="1080"/>
-</tileset>
